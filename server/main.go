abeeb842f165207eff40a0ffc0d7a9fac6502f64
wojci <wojciech.kurdybacha@gmail.com>
2025-07-02 00:30:56 +0200

    add gotmux command layer and ability to attach/detach

--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"log"
 	"syscall"
-
+	"net"
 	"github.com/charmbracelet/x/conpty"
 	"github.com/Microsoft/go-winio"
 )
@@ -19,6 +19,14 @@ const (
 var (
 	currentPty    *conpty.ConPty
 	currentHandle syscall.Handle
+)
+var (
+	outConn net.Conn
+	inConn net.Conn
+)
+var (
+	inListener net.Listener
+	outListener net.Listener
 )
 
 func createConPty(width, height int) (*conpty.ConPty, error) {
@@ -47,39 +55,12 @@ func spawnProcess(pty *conpty.ConPty, cmd string, attr *syscall.ProcAttr) (int, 
 	return pid, handle, nil
 }
 
-func startNamedPipeCommunication(pty *conpty.ConPty) {
-	inListener, err := winio.ListenPipe(inputPipeName, nil)
-	if err != nil {
-		log.Fatalf("Failed to create input pipe: %v", err)
-	}
-	defer inListener.Close()
-
-	outListener, err := winio.ListenPipe(outputPipeName, nil)
-	if err != nil {
-		log.Fatalf("Failed to create output pipe: %v", err)
-	}
-	defer outListener.Close()
-
-	log.Println("Waiting for client to connect to output pipe...")
-	outConn, err := outListener.Accept()
-	if err != nil {
-		log.Fatalf("Output pipe accept failed: %v", err)
-	}
-	defer outConn.Close()
-	log.Println("Client connected to output pipe")
-
-	log.Println("Waiting for client to connect to input pipe...")
-	inConn, err := inListener.Accept()
-	if err != nil {
-		log.Fatalf("Input pipe accept failed: %v", err)
-	}
-	defer inConn.Close()
-	log.Println("Client connected to input pipe")
-
+func attachToTerminal() {
+	log.Println("PTY session started. Awaiting pipe communication.")
 	go func() {
 		buf := make([]byte, 4096)
 		for {
-			n, err := pty.Read(buf)
+			n, err := currentPty.Read(buf)
 			if err != nil {
 				if err != io.EOF {
 					log.Printf("PTY read error: %v", err)
@@ -100,11 +81,18 @@ func startNamedPipeCommunication(pty *conpty.ConPty) {
 	scanner := bufio.NewScanner(inConn)
 	for scanner.Scan() {
 		cmd := scanner.Text()
-		if cmd == "exit" {
+		switch cmd{
+		case "exit":
 			log.Println("Received 'exit' command. Shutting down.")
-			break
+			disposeOfCurrentTerminal()
+			return
+		case "detach":
+			log.Println("Received 'detach' command. Detaching.")
+			return 
 		}
-		_, err := pty.Write([]byte(cmd + "\r"))
+
+		
+		_, err := currentPty.Write([]byte(cmd + "\r"))
 		if err != nil {
 			log.Printf("Failed to write to PTY: %v", err)
 			break
@@ -112,26 +100,90 @@ func startNamedPipeCommunication(pty *conpty.ConPty) {
 	}
 }
 
-func main() {
-	fmt.Println("Starting gotmux server...")
+func disposeOfCurrentTerminal() {
+	currentPty.Close()
+	syscall.CloseHandle(currentHandle)
+}
 
+func startNewTerminal() {
 	pty, err := createConPty(80, 30)
 	if err != nil {
 		log.Fatalf("Failed to create PTY: %v", err)
 	}
-	defer pty.Close()
 	currentPty = pty
 
-	attr := setupChildProcess(pty)
-	_, handle, err := spawnProcess(pty, `C:\Windows\System32\cmd.exe`, attr)
+	attr := setupChildProcess(currentPty)
+	_, handle, err := spawnProcess(currentPty, `C:\Windows\System32\cmd.exe`, attr)
 	if err != nil {
 		log.Fatalf("Failed to start shell: %v", err)
 	}
 	currentHandle = syscall.Handle(handle)
-	defer syscall.CloseHandle(currentHandle)
+	attachToTerminal()
+}
 
-	log.Println("PTY session started. Awaiting pipe communication.")
-	startNamedPipeCommunication(pty)
+func startNamedPipeCommunication() error {
+	var err error
+	inListener, err = winio.ListenPipe(inputPipeName, nil)
+	if err != nil {
+		return err
+	}
 
+	outListener, err = winio.ListenPipe(outputPipeName, nil)
+	if err != nil {
+		return err
+	}
+
+
+	log.Println("Waiting for client to connect to output pipe...")
+	outConn, err = outListener.Accept()
+	if err != nil {
+		return err
+	}
+
+	log.Println("Client connected to output pipe")
+
+	log.Println("Waiting for client to connect to input pipe...")
+	inConn, err = inListener.Accept()
+	if err != nil {
+		return err
+	}
+
+	log.Println("Client connected to input pipe")
+	return nil
+
+}
+
+func startServerCommandLoop() {
+	scanner := bufio.NewScanner(inConn)
+	for scanner.Scan() {
+		cmd := scanner.Text()
+		switch cmd{
+			case "runCMD":
+				startNewTerminal()
+			case "reattach":
+				attachToTerminal()
+			case "exit":
+				log.Println("Received 'exit' command. Shutting down.")
+				return 
+			
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Starting gotmux server...")
+
+	err := startNamedPipeCommunication()
+
+	if err != nil{
+		log.Fatalf("Failed to start named pipe communication: %v", err)
+	}
+
+	defer inListener.Close()
+	defer outListener.Close()
+	defer outConn.Close()
+	defer inConn.Close()
+	startServerCommandLoop()
+	// startNewTerminal()
 	log.Println("Session ended.")
 }
