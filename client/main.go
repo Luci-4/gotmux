779aaa825d216a8d0fd2c5e29feca5efa5b72aca
wojci <wojciech.kurdybacha@gmail.com>
2025-07-02 11:53:33 +0200

    add control pipe for communication and clear screen sequence

--- client/main.go
+++ client/main.go
@@ -4,17 +4,33 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
-
+	"log"
+	"golang.org/x/term"
 	"github.com/Microsoft/go-winio"
 )
 
 const (
 	inputPipe  = `\\.\pipe\gotmux_input`
 	outputPipe = `\\.\pipe\gotmux_output`
+	controlPipe = `\\.\pipe\gotmux_control`
 )
 
 func main() {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		log.Fatalf("failed to set terminal raw mode: %v", err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	ctrlConn, err := winio.DialPipe(controlPipe, nil)
+
+	if err != nil {
+		fmt.Printf("Failed to connect to control pipe: %v\n", err)
+		return
+	}
+	defer ctrlConn.Close()
+	log.Println("Connected to control pipe")
+
 	out, err := winio.DialPipe(outputPipe, nil)
 	if err != nil {
 		fmt.Printf("Failed to connect to output pipe: %v\n", err)
@@ -22,12 +38,15 @@ func main() {
 	}
 	defer out.Close()
 
-	in, err := winio.DialPipe(inputPipe, nil)
+	log.Println("Connected to output pipe")
+	inConn, err := winio.DialPipe(inputPipe, nil)
 	if err != nil {
 		fmt.Printf("Failed to connect to input pipe: %v\n", err)
 		return
 	}
-	defer in.Close()
+	defer inConn.Close()
+
+	log.Println("Connected to input pipe")
 
 	go func() {
 		buf := make([]byte, 4096) 
@@ -45,14 +64,38 @@ func main() {
 		}
 	}()
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text)
-		if text == "exit" {
-			in.Write([]byte("exit\n"))
-			break
-		}
-		in.Write([]byte(text + "\n"))
-	}
+	inReader := bufio.NewReader(os.Stdin)
+    controlWriter := ctrlConn
+    rawWriter := inConn 
+
+    for {
+        b, err := inReader.ReadByte()
+        if err != nil {
+            break
+        }
+
+        if b == 7 { // Ctrl+G (ASCII 7), 
+            term.Restore(int(os.Stdin.Fd()), oldState)
+
+            fmt.Print("\nControl command mode. Enter command: ")
+            lineReader := bufio.NewReader(os.Stdin)
+            cmdLine, err := lineReader.ReadString('\n')
+            if err != nil {
+                fmt.Println("Error reading command:", err)
+            } else {
+                controlWriter.Write([]byte(cmdLine))
+            }
+
+            oldState, err = term.MakeRaw(int(os.Stdin.Fd()))
+            if err != nil {
+                panic(err)
+            }
+            continue
+        }
+
+        _, err = rawWriter.Write([]byte{b})
+        if err != nil {
+            break
+        }
+    }
 }
--- server/main.go
+++ server/main.go
@@ -14,6 +14,7 @@ import (
 const (
 	inputPipeName  = `\\.\pipe\gotmux_input`
 	outputPipeName = `\\.\pipe\gotmux_output`
+	controlPipeName = `\\.\pipe\gotmux_control`
 )
 
 var (
@@ -23,11 +24,27 @@ var (
 var (
 	outConn net.Conn
 	inConn net.Conn
+	controlConn net.Conn
 )
+
+
 var (
 	inListener net.Listener
 	outListener net.Listener
+	controlListener net.Listener
 )
+
+var (
+    attachStopChan chan struct{}
+)
+
+
+func stopAttach() {
+    if attachStopChan != nil {
+        close(attachStopChan)
+        attachStopChan = nil
+    }
+}
 
 func createConPty(width, height int) (*conpty.ConPty, error) {
 	pty, err := conpty.New(width, height, 0)
@@ -56,48 +73,52 @@ func spawnProcess(pty *conpty.ConPty, cmd string, attr *syscall.ProcAttr) (int, 
 }
 
 func attachToTerminal() {
-	log.Println("PTY session started. Awaiting pipe communication.")
-	go func() {
-		buf := make([]byte, 4096)
-		for {
-			n, err := currentPty.Read(buf)
-			if err != nil {
-				if err != io.EOF {
-					log.Printf("PTY read error: %v", err)
-				}
-				break
-			}
-			if n > 0 {
+	attachStopChan = make(chan struct{})
+    log.Println("PTY session started. Forwarding data.")
 
-				_, err := outConn.Write(buf[:n])
+    go func() {
+        buf := make([]byte, 4096)
+        for {
+			select {
+			case <-attachStopChan:
+				return
+
+			default:
+				n, err := currentPty.Read(buf)
 				if err != nil {
-					log.Printf("Failed to write to output pipe: %v", err)
+					if err != io.EOF {
+						log.Printf("PTY read error: %v", err)
+					}
 					break
 				}
+				if n > 0 {
+					_, err := outConn.Write(buf[:n])
+					if err != nil {
+						log.Printf("Failed to write to output pipe: %v", err)
+						break
+					}
+				}
 			}
-		}
-	}()
+        }
+    }()
 
-	scanner := bufio.NewScanner(inConn)
-	for scanner.Scan() {
-		cmd := scanner.Text()
-		switch cmd{
-		case "exit":
-			log.Println("Received 'exit' command. Shutting down.")
-			disposeOfCurrentTerminal()
-			return
-		case "detach":
-			log.Println("Received 'detach' command. Detaching.")
-			return 
-		}
-
-		
-		_, err := currentPty.Write([]byte(cmd + "\r"))
-		if err != nil {
-			log.Printf("Failed to write to PTY: %v", err)
-			break
-		}
-	}
+    buf := make([]byte, 4096)
+    for {
+        n, err := inConn.Read(buf)
+        if err != nil {
+            if err != io.EOF {
+                log.Printf("Input pipe read error: %v", err)
+            }
+            break
+        }
+        if n > 0 {
+            _, err := currentPty.Write(buf[:n])
+            if err != nil {
+                log.Printf("Failed to write to PTY: %v", err)
+                break
+            }
+        }
+    }
 }
 
 func disposeOfCurrentTerminal() {
@@ -122,68 +143,104 @@ func startNewTerminal() {
 }
 
 func startNamedPipeCommunication() error {
-	var err error
-	inListener, err = winio.ListenPipe(inputPipeName, nil)
-	if err != nil {
-		return err
-	}
+    var err error
 
-	outListener, err = winio.ListenPipe(outputPipeName, nil)
-	if err != nil {
-		return err
-	}
+    inListener, err = winio.ListenPipe(inputPipeName, nil)
+    if err != nil {
+        return err
+    }
 
+    outListener, err = winio.ListenPipe(outputPipeName, nil)
+    if err != nil {
+        return err
+    }
 
-	log.Println("Waiting for client to connect to output pipe...")
-	outConn, err = outListener.Accept()
-	if err != nil {
-		return err
-	}
+    err = startControlPipe()
+    if err != nil {
+        return err
+    }
 
-	log.Println("Client connected to output pipe")
+    log.Println("Waiting for client to connect to output pipe...")
+    outConn, err = outListener.Accept()
+    if err != nil {
+        return err
+    }
+    log.Println("Client connected to output pipe")
 
-	log.Println("Waiting for client to connect to input pipe...")
-	inConn, err = inListener.Accept()
-	if err != nil {
-		return err
-	}
+    log.Println("Waiting for client to connect to input pipe...")
+    inConn, err = inListener.Accept()
+    if err != nil {
+        return err
+    }
+    log.Println("Client connected to input pipe")
 
-	log.Println("Client connected to input pipe")
-	return nil
-
+    return nil
 }
 
-func startServerCommandLoop() {
-	scanner := bufio.NewScanner(inConn)
-	for scanner.Scan() {
-		cmd := scanner.Text()
-		switch cmd{
-			case "runCMD":
-				startNewTerminal()
-			case "reattach":
-				attachToTerminal()
-			case "exit":
-				log.Println("Received 'exit' command. Shutting down.")
-				return 
-			
-		}
-	}
+func startControlPipe() error {
+    var err error
+    controlListener, err = winio.ListenPipe(controlPipeName, nil)
+    if err != nil {
+        return err
+    }
+
+    log.Println("Waiting for client to connect to control pipe...")
+    controlConn, err = controlListener.Accept()
+    if err != nil {
+        return err
+    }
+
+    log.Println("Client connected to control pipe")
+    return nil
+}
+
+func controlCommandLoop() {
+    scanner := bufio.NewScanner(controlConn)
+    for scanner.Scan() {
+        cmd := scanner.Text()
+		log.Println(cmd)
+        switch cmd {
+        case "exit":
+            log.Println("Received exit command")
+            disposeOfCurrentTerminal()
+            return
+        case "detach":
+            log.Println("Received detach command")
+			stopAttach()
+			clearSequence := "\x1b[2J\x1b[H"
+			_, err := outConn.Write([]byte(clearSequence))
+			if err != nil {
+				log.Printf("Failed to send clear screen sequence: %v", err)
+			}
+		case "reattach":
+            log.Println("Received reattach command")
+			go attachToTerminal()
+        case "runCMD":
+            log.Println("Received runCMD command")
+            go startNewTerminal()
+        default:
+            log.Printf("Unknown control command: %s", cmd)
+        }
+    }
 }
 
 func main() {
-	fmt.Println("Starting gotmux server...")
+    fmt.Println("Starting gotmux server...")
 
-	err := startNamedPipeCommunication()
+    err := startNamedPipeCommunication()
+    if err != nil {
+        log.Fatalf("Failed to start named pipe communication: %v", err)
+    }
 
-	if err != nil{
-		log.Fatalf("Failed to start named pipe communication: %v", err)
-	}
+    defer inListener.Close()
+    defer outListener.Close()
+    defer controlListener.Close()
 
-	defer inListener.Close()
-	defer outListener.Close()
-	defer outConn.Close()
-	defer inConn.Close()
-	startServerCommandLoop()
-	// startNewTerminal()
-	log.Println("Session ended.")
+    defer outConn.Close()
+    defer inConn.Close()
+    defer controlConn.Close()
+
+    go controlCommandLoop()
+
+    select {} 
 }
